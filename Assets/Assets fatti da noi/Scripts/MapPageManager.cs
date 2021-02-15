f8b59b58770f1303c8c9930c6f7893a888ddd9e1
Anotnio the Gull <abenfante97@gmail.com>
2021-02-15 16:02:56 +0100

    Mappa quasi funzionante

---
+++ Assets/Assets fatti da noi/Scripts/MapPageManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapPageManager : MonoBehaviour
+{
+	public Renderer noSelection;
+
+	private void OnMouseEnter()
+	{
+		GetComponentInChildren<Renderer>().enabled = true;
+		noSelection.enabled = false;
+	}
+
+	private void OnMouseExit()
+	{
+		GetComponentInChildren<Renderer>().enabled = false;
+		noSelection.enabled = true;
+	}
+}
---
+++ Assets/MapManagerMaster.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapManagerMaster : MonoBehaviour
+{
+	public bool _mapPermission;
+	private bool mapInput;
+	public bool mapState;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		mapState = GetComponent<Animator>().GetBool("Mappa su");
+
+		GamePermissionsManager.MovePermission = !mapState;
+
+		Debug.Log("permmesso movimento" + GamePermissionsManager.MovePermission);
+
+		if (Input.GetKeyDown(KeyCode.M) && _mapPermission)
+		{
+			GetComponent<Animator>().SetBool("Mappa su", !mapState);
+
+		}
+
+
+		Debug.Log("Cursor visible: " + Cursor.visible + " |Cursor lock: " + Cursor.lockState);
+    }
+
+	
+}
