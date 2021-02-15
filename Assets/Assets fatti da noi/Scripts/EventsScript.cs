f3b834c7c9114dd2c9324d0f1d9d25de9062a8f2
Bobbe <s287295@studenti.polito.it>
2021-02-15 20:12:29 +0100

    aggiungo classe Events

---
+++ Assets/Assets fatti da noi/Scripts/EventsScript.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventsScript : MonoBehaviour
+{   //Int32.Parse(input)
+	public TextAsset eventFile;
+
+	//variabili
+	List<StateInformation> stateInformation;
+	string[] linesFile;
+
+	void Start()
+	{
+		stateInformation = new List<StateInformation>();
+		linesFile = eventFile.text.Split("\n"[0]);
+		
+		foreach (string str in linesFile)
+		{
+			string[] vecTemp = str.Split('#');
+
+			stateInformation.Add(new StateInformation(numberInBool(Int32.Parse(vecTemp[0])),numberInBool(Int32.Parse(vecTemp[1])),
+				numberInBool(Int32.Parse(vecTemp[2])),numberInBool(Int32.Parse(vecTemp[3])),numberInBool(Int32.Parse(vecTemp[4])),
+				numberInBool(Int32.Parse(vecTemp[5])),numberInBool(Int32.Parse(vecTemp[6])),numberInBool(Int32.Parse(vecTemp[7])),
+				numberInBool(Int32.Parse(vecTemp[8])),Int32.Parse(vecTemp[9]), Int32.Parse(vecTemp[10]),numberInBool(Int32.Parse(vecTemp[11])),
+				numberInBool(Int32.Parse(vecTemp[12])), vecTemp[13]));
+
+		
+		}
+
+		Debug.Log(stateInformation[0].ToString());
+	}
+
+
+	public bool numberInBool (int number)
+	{
+		bool valore=true;
+		if (number == 0)
+		{
+			valore = false;
+		}else if(number == 1)
+		{
+			valore = true;
+		}
+
+		return valore;
+	}
+
+}
---
+++ Assets/Assets fatti da noi/Scripts/StateInformation.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StateInformation
+{
+bool flyPermission;
+bool movePermission;
+bool gatherPermission;
+bool pickUpPermission;
+bool objectPermission;
+bool talkPermissionTartaruga1;
+bool talkPermissionTartaruga2;
+bool talkPermissionUmano;
+bool talkPermissionCapitano;
+int loadDialogueTartaruga1;
+int loadDialogueTartaruga2;
+bool spawnTartaruga1;
+bool spawnTartaruga2;
+string tutorialText;
+
+	public StateInformation(bool flyPermission,bool movePermission,bool gatherPermission,bool pickUpPermission,bool objectPermission,
+	bool talkPermissionTartaruga1,bool talkPermissionTartaruga2,bool talkPermissionUmano,bool talkPermissionCapitano,int loadDialogueTartaruga1,int loadDialogueTartaruga2,
+	 bool spawnTartaruga1, bool spawnTartaruga2, string tutorialText)
+	{
+		this.flyPermission = flyPermission;
+		this.movePermission = movePermission;
+		this.gatherPermission = gatherPermission;
+		this.pickUpPermission = pickUpPermission;
+		this.objectPermission = objectPermission;
+		this.talkPermissionTartaruga1 = talkPermissionTartaruga1;
+		this.talkPermissionTartaruga2 = talkPermissionTartaruga2;
+		this.talkPermissionUmano = talkPermissionUmano;
+		this.talkPermissionCapitano = talkPermissionCapitano;
+		this.loadDialogueTartaruga1 = loadDialogueTartaruga1;
+		this.loadDialogueTartaruga2 = loadDialogueTartaruga2;
+		this.spawnTartaruga1 = spawnTartaruga1;
+		this.spawnTartaruga2 = spawnTartaruga2;
+		this.tutorialText=tutorialText;
+	}
+
+	public override string ToString()
+	{
+		return("flyPermission=" + this.flyPermission + "" + "movePermission=" + this.movePermission +
+			"" + "gatherPermission=" + this.gatherPermission + "" + "pickUpPermission=" + this.pickUpPermission + "" +
+			"objectPermission=" + this.objectPermission + "" + "talkPermissionTartaruga1=" + this.talkPermissionTartaruga1 + "" +
+			"talkPermissionTartaruga2=" + this.talkPermissionTartaruga2+ "" + "spawnTartaruga1=" + this.spawnTartaruga1+ "" +
+			"spawnTartaruga2=" + this.spawnTartaruga2 + "" + "tutorialText" + this.tutorialText);
+
+	}
+}
