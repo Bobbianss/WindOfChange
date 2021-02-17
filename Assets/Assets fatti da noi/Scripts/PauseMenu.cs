b4ce16e8248d3fb75f9bc7065b6cf9e39859ea18
Bobbe <s287295@studenti.polito.it>
2021-02-17 02:38:38 +0100

    Add Video with Sound, menuInGame finish

---
+++ Assets/Assets fatti da noi/Scripts/PauseMenu.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject pausePanel;
+    [HideInInspector]
+    public bool isPaused;
+
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            isPaused = !isPaused;
+            if (isPaused)
+            {
+                pausePanel.SetActive(true);
+            }
+            else
+            {
+                pausePanel.SetActive(false);
+            }
+        }
+
+    }
+
+	public void resume()
+	{
+		pausePanel.SetActive(false);
+		isPaused = false;
+	}
+
+	public void exit()
+	{
+		Debug.Log("DA IMPLEMENTARE L'EXIT");
+		Application.Quit();
+	}
+}
